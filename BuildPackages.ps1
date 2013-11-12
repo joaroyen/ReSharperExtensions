a6c493fd3b36593cb97d05fb5e83f7918ba5a506
Matt Ellis <m.t.ellis@gmail.com>
2013-11-12 13:52:17 +0000

    Support for ReSharper 8.1

--- BuildPackages.ps1
+++ BuildPackages.ps1
@@ -2,7 +2,7 @@ msbuild .\ReSharperExtensions.sln /p:Configuration=Release
 
 pushd Deploy
 
-nuget pack JoarOyen.ReSharperExtensions.Macros.8.0.0.1.nuspec
-nuget pack JoarOyen.ReSharperExtensions.Livetemplates.8.0.0.1.nuspec
+nuget pack JoarOyen.ReSharperExtensions.Macros.nuspec
+nuget pack JoarOyen.ReSharperExtensions.Livetemplates.nuspec
 
 popd
